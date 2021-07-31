ff4f2b4702f4dbc69e1756ef942502a59db2023c
lursh <53827431+lursh@users.noreply.github.com>
2021-07-31 01:08:48 -0400

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 public class test{
 
 	public static void main(String[] args){
-		System.out.println("Hello world..i'm from github23456(webhook)23");
+		System.out.println("Hello world..i'm from githublatest(webhook)23");
 	}
 
 }
